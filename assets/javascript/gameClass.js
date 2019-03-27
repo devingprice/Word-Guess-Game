5fb6116ad16d430ada36cc61a6f08e9ad0118cdc
Devin Price <devingprice@gmail.com>
2019-03-26 23:54:40 -0400

    Updated styles and update method

--- assets/javascript/gameClass.js
+++ assets/javascript/gameClass.js
@@ -1,19 +1,24 @@
 class ScreenCapQuiz {
-    constructor(height, width, words, gameElem) {
-        this.height = height;
-        this.width = width;
-
+    constructor(words, gameElem) {
         this.gameElem = gameElem;
+
         this.finished = false;
         this.gameReady = true;
 
         this.wins = 0;
         this.strikesRemaining = 3;
         this.lettersGuessed = [];
-        this.words = words;
-        this.index = 0;
 
-        var messageHTML = '';
+        this.index = 0;
+        //this.words = words; /*
+        this.words = words.map( movieObj => {
+            return movieObj.title;
+        });
+        this.images = words.map( movieObj => {
+            return movieObj.image;
+        });
+
+        this.messageHTML = '';
     }
 
     runGame(keyPressed){
@@ -21,18 +26,24 @@ class ScreenCapQuiz {
             this.gameReady=false;
             console.log('start playing')
         } else if (this.isValidKey(keyPressed)){
-            this.lettersGuessed.push(keyPressed);
+
+            if(this.lettersGuessed.indexOf(keyPressed) === -1){
+                this.lettersGuessed.push(keyPressed);
+            }
+
             if(!this.keyInWord(keyPressed, this.words[this.index])){
                 this.strikesRemaining -= 1;
             }
             if(this.strikesRemaining === 0){
                 this.lose();
             }
+
             if(this.guessedAllBool(this.words[this.index], this.lettersGuessed)){
                 this.win();
             }
         }
-        this.putResultsOnScreen();
+        //this.putResultsOnScreen();
+        this.putResultsInPlaces();
         if(this.gameReady){
             this.reset();
             this.nextWord();
@@ -62,7 +73,7 @@ class ScreenCapQuiz {
     lose(){
         console.log('lose');
         this.gameReady = true;
-        this.messageHTML = "<div>"+this.words[this.index]+"</div><div>You lost!</div><div>Press Any Key to Continue</div>";
+        this.messageHTML = "<div>You lost!</div><div> Answer was: "+this.words[this.index]+"</div><div>Press Any Key to Continue</div>";
     }
     win(){
         console.log('win');
@@ -80,61 +91,171 @@ class ScreenCapQuiz {
             this.index++
         } else {
             console.log('finished')
-            this.messageHTML = "You have finished all of the words"
             this.finished = true;
-            this.gameElem.innerHTML += this.messageHTML;
+            //this.gameElem.innerHTML += this.messageHTML;
+            var messageContainer = document.getElementById('message-container');
+            messageContainer.innerHTML = messageContainer.innerHTML.replace(
+                "Press Any Key to Continue",
+                "You have finished all of the words"
+            )
         }
     }
-    putResultsOnScreen(){
-        var strikesString = "<div>Number of Guesses remaining: " 
-            + this.strikesRemaining + 
-            "</div>";
-        
-        var blanksString = "<div>"+
-            this.createBlanksString(this.words[this.index], this.lettersGuessed) +
-            "</div>";
-        
-        gameElem.innerHTML=(
-            strikesString +
-            this.createLettersGuessedHTML(this.lettersGuessed) +
-            blanksString +
-            this.createTileGroupHTML(this.words[this.index], this.lettersGuessed) +
-            this.messageHTML
-        );
-    }
-    createBlanksString(word, lettersGuessed){
-        var resultString = '';
-        for(var i=0;i<word.length;i++){
-            if(this.keyInWord(word[i], lettersGuessed.join(""))){
-                resultString += word[i]+" ";
-            } else if(word[i]===" "){
-                resultString += "&nbsp;&nbsp;&nbsp;";
-            } else {
-                resultString += "_ ";
-            }
-        }
-        return resultString;
-    }
-    createLettersGuessedHTML(lettersArray){
-        var resultHTML = '<div class="guessed tile-container">';
-        for(var i=0; i<lettersArray.length; i++){
-            resultHTML += "<div class='faded tile tile--small'>" + lettersArray[i] + "</div>";
-        }
-        resultHTML += "</div>"
-        return resultHTML;
-    }
-    createTileGroupHTML(word, lettersGuessed){
-        var resultHTML = '<div>';
+    createSolutionHTML(word, lettersGuessed){
+        var resultHTML = "";
         var individualWords = word.split(' ');
         for(var i=0; i<individualWords.length; i++){
             var wordContainer = "<div class='tile-container'>";
             
             for(var j=0; j<individualWords[i].length; j++){
-    
+                
                 if(this.keyInWord(individualWords[i][j], lettersGuessed.join(""))){
                     wordContainer += "<div class='wooden tile'>" + individualWords[i][j] + "</div>";
-                } else if(word[i]===" "){ //shouldnt reach this after splitting, dead code
-                    wordContainer += "<div class='wooden tile'>" + " " + "</div>";
+                } else {
+                    wordContainer += "<div class='empty tile'>"  + "</div>";
+                }
+            }
+    
+            wordContainer += "</div>"
+            resultHTML += wordContainer;
+        }
+        return resultHTML;
+    }
+
+    putResultsInPlaces(){
+        if(this.messageHTML !== ''){
+            document.getElementById('image-container').innerHTML = (
+                "<img class='screenshot-img' src='"+ this.images[this.index] + "'>" +
+                "<div class='message-container' id='message-container'>"+ this.messageHTML + "</div>"
+            )
+        } else {
+            document.getElementById('image-container').innerHTML = (
+                "<img class='screenshot-img' src='"+ this.images[this.index] + "'>"
+            )
+        }
+        document.getElementById('strikesNum').innerText = this.strikesRemaining;
+        
+        var incorrectLetters = '';
+        for(var i=0; i<this.lettersGuessed.length; i++){
+            if(!this.keyInWord(this.lettersGuessed[i], this.words[this.index])){
+                incorrectLetters += "<div class='faded tile tile--small'>" + this.lettersGuessed[i] + "</div>";
+            }
+        }
+        
+        document.getElementById('guessedWords').innerHTML = incorrectLetters;
+        document.getElementById('winsNum').innerText = this.wins;
+
+        document.getElementById('solution-container').innerHTML = this.createSolutionHTML(this.words[this.index],this.lettersGuessed);
+    }
+}
+
+
+var sources = [
+    { 
+        image: 'https://img.buzzfeed.com/buzzfeed-static/static/2018-12/16/16/enhanced/buzzfeed-prod-web-06/enhanced-8749-1544995921-7.jpg?downsize=800:*&output-format=auto&output-quality=auto',
+        title: 'Peter Pan'
+    },
+    {
+        image: 'https://img.buzzfeed.com/buzzfeed-static/static/2018-12/16/16/enhanced/buzzfeed-prod-web-01/enhanced-14589-1544994583-1.jpg?downsize=800:*&output-format=auto&output-quality=auto',
+        title: 'Snow White and the Seven Dwarfs'
+    },
+    { 
+        image: 'https://img.buzzfeed.com/buzzfeed-static/static/2018-12/16/16/enhanced/buzzfeed-prod-web-06/enhanced-7509-1544994782-1.jpg?downsize=800:*&output-format=auto&output-quality=auto',
+        title: 'Mulan'
+    },
+    { 
+        image: 'https://img.buzzfeed.com/buzzfeed-static/static/2018-12/16/16/enhanced/buzzfeed-prod-web-04/enhanced-7887-1544995461-1.jpg?downsize=800:*&output-format=auto&output-quality=auto',
+        title: 'The Little Mermaid'
+    },
+    { 
+        image: 'https://img.buzzfeed.com/buzzfeed-static/static/2018-12/16/16/enhanced/buzzfeed-prod-web-06/enhanced-8487-1544995699-1.jpg?downsize=800:*&output-format=auto&output-quality=auto',
+        title: 'Sleeping Beauty'
+    }
+]
+
+var gameElem = document.getElementById('game');
+
+var game = new ScreenCapQuiz(sources, gameElem)
+
+document.onkeydown = function(evt){
+    document.getElementById('startMessage').setAttribute("style", "display:none");
+    document.getElementById('game').setAttribute("style", "display:flex");
+    
+    if(!game.finished){
+        game.runGame(evt.key);
+    }
+    
+}
+
+/*
+
+
+    putResultsOnScreen(){
+        var wordsContainer =
+        "<div class='words-container'>" +
+            "<div class='stats-container'>" +
+            this.createStrikesHTML(this.strikesRemaining) +
+            this.createLettersGuessedHTML(this.lettersGuessed) +
+            this.createWinsHTML(this.wins) +
+            "</div>" +
+            //this.createBlanksHTML(this.words[this.index], this.lettersGuessed) +
+            this.createTileGroupHTML(this.words[this.index], this.lettersGuessed) +
+            //this.messageHTML +
+        "</div>";
+
+        var messageContainer = this.createMessageHTML(this.messageHTML);
+
+        var imageContainer = "<div class='image-container'>"+
+        "<img class='screenshot-img' src='"+ this.images[this.index] + "'>" + 
+        messageContainer +
+        "</div>"
+
+        gameElem.innerHTML = "<div class='game-container'>"+ imageContainer + wordsContainer  + "</div>";
+    }
+    createMessageHTML(message){
+        if(message !== ''){
+            return "<div class='message-container'>"+
+            message + "</div>";
+        } else {
+            return ""
+        }
+    }
+    createWinsHTML(wins){
+        return "<div class='wins-container'>" + 
+            "<div class='wins--header'>Wins:</div> " +
+            "<div class='winsNum'>" + wins + "</div>" +
+        "</div>";
+    }
+    createStrikesHTML(strikeNum){
+        return "<div class='strikes-container'>" + 
+            "<div class='stikes--header'>Guesses Remaining:</div> " +
+            "<div class='strikesNum'>" + strikeNum + "</div>" +
+        "</div>";
+    }
+    createLettersGuessedHTML(lettersArray){
+        var resultHTML = "<div class='guessed-container'>"+
+        "<div class='guessed--header'>Incorrect Guesses </div> " +
+        "<div class='tile-container'>";
+        for(var i=0; i<lettersArray.length; i++){
+
+            // only display incorrect guesses
+            if(!this.keyInWord(lettersArray[i], this.words[this.index])){
+                resultHTML += "<div class='faded tile tile--small'>" + lettersArray[i] + "</div>";
+            }
+
+        }
+        resultHTML += "</div></div>"
+        return resultHTML;
+    }
+    createTileGroupHTML(word, lettersGuessed){
+        var resultHTML = "<div class='solution-container'>";
+        var individualWords = word.split(' ');
+        for(var i=0; i<individualWords.length; i++){
+            var wordContainer = "<div class='tile-container'>";
+            
+            for(var j=0; j<individualWords[i].length; j++){
+                
+                if(this.keyInWord(individualWords[i][j], lettersGuessed.join(""))){
+                    wordContainer += "<div class='wooden tile'>" + individualWords[i][j] + "</div>";
                 } else {
                     wordContainer += "<div class='empty tile'>"  + "</div>";
                 }
@@ -146,16 +267,4 @@ class ScreenCapQuiz {
         resultHTML += "</div>"
         return resultHTML;
     }
-}
-
-
-var gameElem = document.getElementById('game');
-
-var game = new ScreenCapQuiz(0,0,['Katy Perry', 'Madonna', 'Snoop'], gameElem)
-
-document.onkeydown = function(evt){
-    if(!game.finished){
-        game.runGame(evt.key);
-    }
-    
-}
+*/
