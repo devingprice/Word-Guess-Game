948a011a468459a82d7f83b578308c8ff4dc6670
devingprice <devingprice@gmail.com>
2019-03-29 11:12:26 -0400

    Change class to object

--- assets/javascript/gameClass.js
+++ assets/javascript/gameClass.js
@@ -1,99 +1,99 @@
-class ScreenCapQuiz {
-    constructor(words, gameElem) {
-        this.gameElem = gameElem;
-
-        this.finished = false;
-        this.gameReady = true;
-
-        this.wins = 0;
-        this.strikesRemaining = 3;
-        this.lettersGuessed = [];
-
-        this.index = 0;
-        this.data = words;
-        this.title = this.data[this.index].title;
-        this.image = this.data[this.index].image;
-       
-        this.messageHTML = '';
+function isValidKey(key){
+    return /^[a-zA-Z0-9]$/i.test(key); //from https://forums.asp.net/t/1289763.aspx?Regular+Expression+Validator+Letters+and+Numbers+only+
+}
+function keyInWord(key, word){
+    var lowerCaseWord = word.toLowerCase();
+    var lowerCaseLetter = key.toLowerCase();
+    return lowerCaseWord.indexOf(lowerCaseLetter) !== -1;
+}
+function guessedAll(word, letters){
+    for(var i=0;i<word.length;i++){
+        if (!( letters.indexOf(word[i].toUpperCase()) !== -1 ||
+            letters.indexOf(word[i].toLowerCase()) !== -1
+        )){
+            if( word[i] !== " "){
+                return false;
+            }
+        }
     }
+    return true;
+}
+var audioPlayer = {
+    right: function (){
+        document.getElementById('audio-right').play();
+    },
+    wrong: function (){
+        document.getElementById('audio-wrong').play();
+    },
+    victory: function(){
+        document.getElementById('audio-win').play();
+    }
+}
 
-    runGame(keyPressed){
-        if(this.gameReady){
+var quizObject = {
+    finished : false,
+    gameReady : true,
+    wins : 0,
+    strikesRemaining : 3,
+    lettersGuessed : [],
+    index : 0,
+    data : sources,
+    title : sources[0].title,//just initializing  this.data[this.index].title,
+    image : sources[0].image,//just initializing  this.data[this.index].image,
+    messageHTML : '',
+    runGame : function(keyPressed){
+        if(this.gameReady){ // stop game until keypress at start or in between rounds
             this.gameReady=false;
             console.log('start playing')
-        } else if (this.isValidKey(keyPressed)){
+        } else if (isValidKey(keyPressed)){ //if key is letter or num
 
-            if(this.lettersGuessed.indexOf(keyPressed) === -1){
+            if(this.lettersGuessed.indexOf(keyPressed) === -1){ // if you havent already typed this
                 this.lettersGuessed.push(keyPressed);  
+                console.log(this.lettersGuessed)
+            } else { //if new letter
+                if(!keyInWord(keyPressed, this.title)){ //if not in title
+                    console.log("keypressed:",keyPressed, this.title)
+                    console.log(keyInWord(keyPressed, this.title))
+                    this.strikesRemaining -= 1;
+                    audioPlayer.wrong();
+                } else {
+                    audioPlayer.right();
+                }
             }
-            if(!this.keyInWord(keyPressed, this.title)){
-                this.strikesRemaining -= 1;
-                this.playWrong();
-            } else {
-                this.playRight();
-            }
+            
+            // win lose conditions
             if(this.strikesRemaining === 0){
                 this.lose();
-            }
-            if(this.guessedAll(this.title, this.lettersGuessed)){
+            } else if(guessedAll(this.title, this.lettersGuessed)){
                 this.win();
-                this.playVictory();
+                audioPlayer.victory();
             }
         }
         
-        this.putResultsInPlaces();
+        this.putResultsInPlaces(); //display
+        //next round
         if(this.gameReady){
             this.reset();
             this.nextWord();
         }   
-    }
-    
-    isValidKey(key){
-        return /^[a-zA-Z0-9]$/i.test(key); //from https://forums.asp.net/t/1289763.aspx?Regular+Expression+Validator+Letters+and+Numbers+only+
-    }
-    keyInWord(key, word){
-        var lowerCaseWord = word.toLowerCase();
-        var lowerCaseLetter = key.toLowerCase();
-        return lowerCaseWord.indexOf(lowerCaseLetter) !== -1;
-    }
-    guessedAll(word, letters){
-        for(var i=0;i<word.length;i++){
-            if (!( letters.indexOf(word[i].toUpperCase()) !== -1 ||
-                letters.indexOf(word[i].toLowerCase()) !== -1
-            )){
-                if( word[i] !== " "){
-                    return false;
-                }
-            }
-        }
-        return true;
-    }
-    lose(){
+    },
+    lose : function(){
         console.log('lose');
         this.gameReady = true;
         this.messageHTML = "<div>You lost!</div><div> Answer was: "+this.title+"</div><div>Press Any Key to Continue</div>";
-    }
-    playWrong(){
-        document.getElementById('audio-wrong').play();
-    }
-    playRight(){
-        document.getElementById('audio-right').play();
-    }
-    playVictory(){
-        document.getElementById('audio-win').play();
-    }
-    win(){
+    },
+    win : function(){
         console.log('win');
         this.gameReady = true; 
         this.wins++;
         this.messageHTML = "<div>You Won!</div><div>Press Any Key to Continue</div>";
-    }
-    reset(){
+    },
+    reset : function(){
         this.strikesRemaining = 3;
         this.lettersGuessed = [];
         this.messageHTML = '';
-    }
-    nextWord(){
+    },
+    nextWord : function(){
         if(this.index < this.data.length -1){
             this.index++
             this.title = this.data[this.index].title;
@@ -108,16 +108,16 @@ class ScreenCapQuiz {
                 "You have finished all of the words"
             )
         }
-    }
-    createSolutionHTML(word, lettersGuessed){
+    },
+    createSolutionHTML : function(){
         var resultHTML = "";
-        var individualWords = word.split(' ');
+        var individualWords = this.title.split(' ');
         for(var i=0; i<individualWords.length; i++){
             var wordContainer = "<div class='tile-container'>";
             
             for(var j=0; j<individualWords[i].length; j++){
                 
-                if(this.keyInWord(individualWords[i][j], lettersGuessed.join(""))){
+                if(keyInWord(individualWords[i][j], this.lettersGuessed.join(""))){
                     wordContainer += "<div class='wooden tile'>" + individualWords[i][j] + "</div>";
                 } else {
                     wordContainer += "<div class='empty tile'>"  + "</div>";
@@ -128,9 +128,8 @@ class ScreenCapQuiz {
             resultHTML += wordContainer;
         }
         return resultHTML;
-    }
-
-    putResultsInPlaces(){
+    },
+    putResultsInPlaces : function(){
         if(this.messageHTML !== ''){
             document.getElementById('image-container').innerHTML = (
                 "<img class='screenshot-img' src='"+ this.image + "'>" +
@@ -147,9 +146,9 @@ class ScreenCapQuiz {
         
         var incorrectLetters = '';
         for(var i=0; i<this.lettersGuessed.length; i++){
-            if(!this.keyInWord(this.lettersGuessed[i], this.title)){
+            if(!keyInWord(this.lettersGuessed[i], this.title)){
                 incorrectLetters += "<div class='tile '>" + this.lettersGuessed[i] + "</div>";
-            } //used to use faded class   tile--small
+            }
         }
         
         document.getElementById('guessedWords').innerHTML = incorrectLetters;
@@ -158,35 +157,11 @@ class ScreenCapQuiz {
     }
 }
 
-/*
-var sources = [
-    { 
-        image: 'https://img.buzzfeed.com/buzzfeed-static/static/2018-12/16/16/enhanced/buzzfeed-prod-web-06/enhanced-8749-1544995921-7.jpg?downsize=800:*&output-format=auto&output-quality=auto',
-        title: 'Peter Pan'
-    },
-    {
-        image: 'https://img.buzzfeed.com/buzzfeed-static/static/2018-12/16/16/enhanced/buzzfeed-prod-web-01/enhanced-14589-1544994583-1.jpg?downsize=800:*&output-format=auto&output-quality=auto',
-        title: 'Snow White and the Seven Dwarfs'
-    },
-    { 
-        image: 'https://img.buzzfeed.com/buzzfeed-static/static/2018-12/16/16/enhanced/buzzfeed-prod-web-06/enhanced-7509-1544994782-1.jpg?downsize=800:*&output-format=auto&output-quality=auto',
-        title: 'Mulan'
-    },
-    { 
-        image: 'https://img.buzzfeed.com/buzzfeed-static/static/2018-12/16/16/enhanced/buzzfeed-prod-web-04/enhanced-7887-1544995461-1.jpg?downsize=800:*&output-format=auto&output-quality=auto',
-        title: 'The Little Mermaid'
-    },
-    { 
-        image: 'https://img.buzzfeed.com/buzzfeed-static/static/2018-12/16/16/enhanced/buzzfeed-prod-web-06/enhanced-8487-1544995699-1.jpg?downsize=800:*&output-format=auto&output-quality=auto',
-        title: 'Sleeping Beauty'
-    }
-]*/
 
-var gameElem = document.getElementById('game');
-
-var game = new ScreenCapQuiz(sources, gameElem)
+var game = quizObject; 
 
 document.onkeydown = function(evt){
+    //runs every click but just resets this value over and over
     document.getElementById('startMessage').setAttribute("style", "display:none");
     document.getElementById('frame-container').setAttribute("style", "display:flex"); 
     
